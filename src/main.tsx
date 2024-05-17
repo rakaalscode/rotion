72640a580a1014919db0037e1b0224cd66b90ce5
Rakaal <rakaalscode@gmail.com>
2024-05-17 14:31:17 +0700

    adding schema and API

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "sonner";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/components/providers/theme-provider.tsx";
 import { ConvexClientProvider } from "@/components/providers/convex-provider.tsx";
@@ -10,9 +11,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConvexClientProvider>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <Toaster position="bottom-center" />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
       </ThemeProvider>
     </ConvexClientProvider>
   </React.StrictMode>
