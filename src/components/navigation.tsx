aacad7f6e9791f03134e661a8145b3003b3e5632
Rakaal <rakaalscode@gmail.com>
2024-05-21 16:25:12 +0700

    adding setting function

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -22,8 +22,10 @@ import { toast } from "sonner";
 
 import { api } from "@convex/_generated/api";
 import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
 
 export const Navigation = () => {
+  const settings = useSettings();
   const search = useSearch();
   const { pathname } = useLocation();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -145,7 +147,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
           <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
-          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "sonner";
 import { BrowserRouter } from "react-router-dom";
+
 import { ThemeProvider } from "@/components/providers/theme-provider.tsx";
 import { ConvexClientProvider } from "@/components/providers/convex-provider.tsx";
+import { ModalProvider } from "./components/providers/modal.providers.tsx";
 
 import App from "./App.tsx";
 
@@ -12,9 +14,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ConvexClientProvider>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <Toaster position="bottom-center" />
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
+        <ModalProvider />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </ConvexClientProvider>
   </React.StrictMode>
